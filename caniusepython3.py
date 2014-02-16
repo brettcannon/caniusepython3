056e23c3f5430ddbe221a6f5fea684b989b71a33
Brett Cannon <brett@python.org>
2014-02-16 10:59:38 -0500

    Tweak some code formatting

--- caniusepython3.py
+++ caniusepython3.py
@@ -160,13 +160,17 @@ def projects_from_cli(args):
     description = ('Determine if a set of project dependencies will work with '
                    'Python 3')
     parser = argparse.ArgumentParser(description=description)
+    req_help = ('path to a pkg_resources requirements file '
+                '(e.g. requirements.txt)')
     parser.add_argument('--requirements', '-r', nargs='?',
-        help='path to a pkg_resources requirements file (e.g. requirements.txt)')
+                        help=req_help)
+    meta_help = 'path to a PEP 426 metadata file (e.g. PKG-INFO, pydist.json)'
     parser.add_argument('--metadata', '-m', nargs='?',
-        help='path to a PEP 426 metadata file (e.g. PKG-INFO, pydist.json)')
-    parser.add_argument('--projects', '-p', type=lambda arg: arg.split(','), nargs='?',
-        help='a comma-separated list of projects')
-    parser.add_argument('--verbose', '-v', action='store_true', help='verbose output')
+                        help=meta_help)
+    parser.add_argument('--projects', '-p', type=lambda arg: arg.split(','),
+                        nargs='?', help='a comma-separated list of projects')
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='verbose output')
     parsed = parser.parse_args(args)
 
     projects = []
