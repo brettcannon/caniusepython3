bfea09b26b85611bae5052815a62aa804e8e8fd8
Brett Cannon <brett@python.org>
2014-11-28 12:34:03 -0500

    Bring checker in line with Pylint's --py3k

--- caniusepython3/pylint_checker.py
+++ caniusepython3/pylint_checker.py
@@ -27,261 +27,64 @@ from astroid import nodes
 from pylint import checkers, interfaces
 from pylint.checkers import utils
 
-# http://python3porting.com/differences.html
-## Straight-forward ########################
-### No dict.iter*()
-### No list.sort(cmp=)
-## Scoping #################################
-### no sorted(cmp=)
-### list(filter()) or future_builtins.filter()
-### No exception object escaping `except` scope
-### No listcomp variable escaping
-## Don't know how to catch #################
-### indexing bytes
 
-
-class SixChecker(checkers.BaseChecker):
+class StrictPython3Checker(checkers.BaseChecker):
 
     __implements__ = interfaces.IAstroidChecker
 
-    name = 'six'
+    name = 'strictpython3'
     msgs = {
         # Errors for what will syntactically break in Python 3, warnings for
         # everything else.
-        'E6001': ('Use of a print statement',
-                  'print-statement',
-                  'Used when a print statement is found '
-                  '(invalid syntax in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'E6002': ('Use of an exec statement',
-                  'exec-statement',
-                  'Used when an exec statement is found'
-                  '(invalid syntax in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'E6003': ('Parameter unpacking specified',
-                  'parameter-unpacking',
-                  'Used when parameter unpacking is specified for a function'
-                  "(Python 3 doesn't allow it)",
-                  {'maxversion': (3, 0)}),
-        'W6001': ('__getslice__ defined',
-                  'getslice-method',
-                  'Used when a __getslice__ method  is defined '
-                  '(unused in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6002': ('__setslice__ defined',
-                  'setslice-method',
-                  'Used when a __setslice__ method is defined '
-                  '(unused in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6003': ('__cmp__ defined',
-                  'cmp-method',
-                  'Used when a __cmp__ method is defined '
-                  '(unused in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6004': ('__coerce__ defined',
-                  'coerce-method',
-                  'Used when a __coerce__ method is defined '
-                  '(unused in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6005': ('__unicode__ defined',
-                  'unicode-method',
-                  'Used when a __unicode__ method is defined '
-                  '(renamed __str__ in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6006': ('next defined',
-                  'next-method',
-                  "Used when a 'next' method is defined "
-                  '(renamed __next__ in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6007': ('buffer built-in referenced',
-                  'buffer-builtin',
-                  'Used when the buffer() built-in function is referenced '
-                  '(removed in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6008': ('apply built-in referenced',
-                  'apply-builtin',
-                  'Used when the apply() built-in function is referenced '
-                  '(removed in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6009': ('cmp built-in referenced',
-                  'cmp-builtin',
-                  'Used when the cmp() built-in function is referenced '
-                  '(removed in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6010': ('file built-in referenced',
-                  'file-builtin',
-                  'Used when the file() built-in function is referenced '
-                  '(removed in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6011': ('raw_input built-in referenced',
-                  'raw_input-builtin',
-                  'Used when the raw_input() built-in function is referenced '
-                  "(renamed 'input' in Python 3)",
-                  {'maxversion': (3, 0)}),
-        'W6012': ('long built-in referenced',
-                  'long-builtin',
-                  'Used when the long() built-in function is referenced '
-                  '(removed in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6013': ('coerce built-in referenced',
-                  'coerce-builtin',
-                  'Used when the coerce() built-in function is referenced '
-                  '(removed in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6014': ('execfile built-in referenced',
-                  'execfile-builtin',
-                  'Used when the execfile() built-in function is referenced '
-                  '(removed in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6015': ('xrange built-in referenced',
-                  'xrange-builtin',
-                  'Used when the xrange() built-in function is referenced '
-                  "(renamed 'range' in Python 3)",
-                  {'maxversion': (3, 0)}),
-        'W6016': ('unicode built-in referenced',
-                  'unicode-builtin',
-                  'Used when the unicode() built-in function is referenced '
-                  "(renamed 'str' in Python 3)",
-                  {'maxversion': (3, 0)}),
-        'W6017': ('StandardError built-in referenced',
-                  'standarderror-builtin',
-                  'Used when the StandardError built-in exception is referenced '
-                  '(removed in Python 3)',
-                  {'maxversion': (3, 0)}),
-        'W6018': ('map built-in referenced',
+        'W6001': ('map built-in referenced',
                   'map-builtin',
                   'Used when the map built-in function is referenced '
                   '(semantics different in Python 3; '
-                  'use future_builtins.map)',
+                  'use `from six.moves import map`)',
                   {'maxversion': (3, 0)}),
-        'W6019': ('zip built-in referenced',
+        'W6002': ('range built-in referenced',
+                  'range-builtin',
+                  'Used when the range built-in function is referenced '
+                  '(semantics different in Python 3; '
+                  'use `from six.moves import range`)',
+                  {'maxversion': (3, 0)}),
+        'W6003': ('zip built-in referenced',
                   'zip-builtin',
                   'Used when the zip built-in function is referenced '
                   '(semantics different in Python 3; '
-                  'use future_builtins.zip)',
+                  'use `from six.move import zip`)',
                   {'maxversion': (3, 0)}),
-        'W6020': ('division w/o __future__ statement',
-                  'division',
-                  'Used for non-floor division w/o a float literal or '
-                  '``from __future__ import division``'
-                  '(Python 3 returns a float for int division unconditionally)',
-                  {'maxversion': (3, 0)}),
-        'W6021': ('import w/o ``from __future__ import absolute_import``',
-                  'no-absolute-import',
-                  'Used when an import is performed w/o'
-                  '``from __future__ import absolute_import``',
-                  {'maxversion': (3, 0)}),
-        'W6022': ('__metaclass__ assigned',
-                  'metaclass-attribute',
-                  'Assignment to the __metaclass__ attribute'
-                  "(Python 3 sets the metaclass in the class' parameter list)",
-                  {'maxversion': (3, 0)}),
-        'W6023': ('round built-in referenced',
+        'W6004': ('open built-in referenced',
+                  'open-builtin',
+                  'Used when the open built-in function is referenced '
+                  '(semantics different in Python 3; '
+                  'use `from io import open`)',
+                {'maxversion': (3, 0)}),
+        'W6005': ('round built-in referenced',
                   'round-builtin',
                   'Used when the round built-in function is referenced '
                   '(semantics different in Python 3)',
                   {'maxversion': (3, 0)}),
-        'W6024': ('open built-in referenced',
-                  'open-builtin',
-                  'Used when the open built-in function is referenced '
-                  '(semantics different in Python 3; use io.open or codecs.open)',
-                  {'maxversion': (3, 0)}),
     }
 
-    def __init__(self, *args, **kwargs):
-        self._future_division = False
-        self._future_absolute_import = False
-        super(SixChecker, self).__init__(*args, **kwargs)
-
-    @utils.check_messages('print-statement')
-    def visit_print(self, node):
-        self.add_message('print-statement', node=node)
-
-    @utils.check_messages('exec-statement')
-    def visit_exec(self, node):
-        self.add_message('exec-statement', node=node)
-
-    @utils.check_messages('no-absolute-import')
-    def visit_from(self, node):
-        if node.modname == u'__future__' :
-            for name, _ in node.names:
-                if name == u'division':
-                    self._future_division = True
-                elif name == u'absolute_import':
-                    self._future_absolute_import = True
-        elif not self._future_absolute_import:
-            self.add_message('no-absolute-import', node=node)
-
-    @utils.check_messages('no-absolute-import')
-    def visit_import(self, node):
-        if not self._future_absolute_import:
-            self.add_message('no-absolute-import', node=node)
-
-    @utils.check_messages('division')
-    def visit_binop(self, node):
-        if not self._future_division and node.op == u'/':
-            for arg in (node.left, node.right):
-                if isinstance(arg, astroid.Const) and isinstance(arg.value, float):
-                    break
-            else:
-                self.add_message('division', node=node)
-
-    def visit_function(self, node):
-        bad_methods = {'__getslice__': 'getslice-method',
-                       '__setslice__': 'setslice-method',
-                       '__cmp__': 'cmp-method',
-                       '__coerce__': 'coerce-method',
-                       '__unicode__': 'unicode-method',
-                       'next': 'next-method'}
-        if node.is_method() and node.name in bad_methods:
-            self.add_message(bad_methods[node.name], node=node)
-
-    @utils.check_messages('parameter-unpacking')
-    def visit_arguments(self, node):
-        for arg in node.args:
-            if isinstance(arg, nodes.Tuple):
-                self.add_message('parameter-unpacking', node=arg)
+    _changed_builtins = frozenset(['map', 'open', 'range', 'round', 'zip'])
 
     def visit_name(self, node):
         if node.lookup(node.name)[0].name == '__builtin__':
-            bad_builtins = {'buffer': 'buffer-builtin',
-                            'apply': 'apply-builtin',
-                            'cmp': 'cmp-builtin',
-                            'file': 'file-builtin',
-                            'raw_input': 'raw_input-builtin',
-                            'long': 'long-builtin',
-                            'coerce': 'coerce-builtin',
-                            'execfile': 'execfile-builtin',
-                            'xrange': 'xrange-builtin',
-                            'unicode': 'unicode-builtin',
-                            'StandardError': 'standarderror-builtin',
-                            # Built-ins listed below are for semantic reasons
-                            # instead of flat-out missing in Python 3.
-                            'map': 'map-builtin',
-                            'zip': 'zip-builtin',
-                            'round': 'round-builtin',
-                            'open': 'open-builtin',
-                           }
-            if node.name in bad_builtins:
-                self.add_message(bad_builtins[node.name], node=node)
-
-    @utils.check_messages('metaclass-attribute')
-    def visit_assign(self, node):
-        for target in node.targets:
-            if u'__metaclass__' == target.name:
-                self.add_message('metaclass-attribute', node=node)
+            if node.name in self._changed_builtins:
+                self.add_message(node.name + '-builtin', node=node)
 
 
 class UnicodeChecker(checkers.BaseTokenChecker):
 
     __implements__ = interfaces.ITokenChecker
 
-    name = 'unicode'
+    name = 'strictpython3'
     msgs = {
         'W6100': ('native string literal',
                   'native-string',
-                  'Used when a string has no b/u prefix and '
-                  '``from __future__ import unicode_literals`` not found '
+                  'Used when a string has no `b`/`u` prefix and '
+                  '`from __future__ import unicode_literals` is not found '
                   '(strings w/ no prefix in Python 3 are Unicode)',
                   {'maxversion': (3, 0)}),
     }
@@ -311,40 +114,6 @@ class UnicodeChecker(checkers.BaseTokenChecker):
                         return
 
 
-class SyntaxChecker(checkers.BaseTokenChecker):
-
-    __implements__ = interfaces.ITokenChecker
-
-    name = 'six'
-    msgs = {
-        'E6101': ('octal literal',
-                  'octal-literal',
-                  'Used when a octal literal w/ ``0`` prefix is defined '
-                  '(Python 3 uses ``0o``)',
-                  {'maxversion': (3, 0)}),
-        'E6102': ('long literal',
-                  'long-literal',
-                  'Used when a long literal is defined '
-                  '(Python 3 unified int and long)',
-                  {'maxversion': (3, 0)}),
-    }
-
-    def process_tokens(self, tokens):
-        line_num = 1
-        for type_, val, start, end, line in tokens:
-            if type_ in (token.NEWLINE, tokenize.NL):
-                line_num += 1
-            elif type_ == token.STRING:
-                line_num += val.count('\n')
-            elif type_ == token.NUMBER and len(val) > 1:
-                if val.startswith(u'0'):
-                    if not val.startswith(u'0o'):
-                        self.add_message('octal-literal', line=line_num)
-                elif val.endswith((u'L', u'l')):
-                    self.add_message('long-literal', line=line_num)
-
-
 def register(linter):
-    linter.register_checker(SixChecker(linter))
+    linter.register_checker(StrictPython3Checker(linter))
     linter.register_checker(UnicodeChecker(linter))
-    linter.register_checker(SyntaxChecker(linter))
--- caniusepython3/test/test_pylint_checker.py
+++ caniusepython3/test/test_pylint_checker.py
@@ -28,75 +28,9 @@ def python2_only(test):
     return unittest.skipIf(sys.version_info[0] > 2, 'Python 2 only')(test)
 
 
-class SixCheckerTest(testutils.CheckerTestCase):
+class StrictPython3CheckerTest(testutils.CheckerTestCase):
 
-    CHECKER_CLASS = checker.SixChecker
-
-    @python2_only
-    def test_print_statement(self):
-        node = test_utils.extract_node('print "Hello, World!" #@')
-        with self.assertAddsMessages(testutils.Message('print-statement', node=node)):
-            self.checker.visit_print(node)
-
-    def test_method_false_positive(self):
-        node = test_utils.extract_node("""
-        def next():  #@
-            pass""")
-        with self.assertNoMessages():
-            self.checker.visit_function(node)
-
-    def test_getslice_method(self):
-        node = test_utils.extract_node("""
-        class Foo(object):
-            def __getslice__(self, i, j):  #@
-                pass""")
-        with self.assertAddsMessages(testutils.Message('getslice-method', node=node)):
-            self.checker.visit_function(node)
-
-    def test_setslice_method(self):
-        node = test_utils.extract_node("""
-        class Foo(object):
-            def __setslice__(self, i, j, value):  #@
-                pass""")
-        with self.assertAddsMessages(testutils.Message('setslice-method', node=node)):
-            self.checker.visit_function(node)
-
-    def test_cmp_method(self):
-        node = test_utils.extract_node("""
-        class Foo(object):
-            def __cmp__(self, other):  #@
-                pass""")
-        with self.assertAddsMessages(testutils.Message('cmp-method', node=node)):
-            self.checker.visit_function(node)
-
-    def test_coerce_method(self):
-        node = test_utils.extract_node("""
-        class Foo(object):
-            def __coerce__(self, other):  #@
-                pass""")
-        with self.assertAddsMessages(testutils.Message('coerce-method', node=node)):
-            self.checker.visit_function(node)
-
-    def test_unicode_method(self):
-        node = test_utils.extract_node("""
-        class Foo(object):
-            def __unicode__(self, other):  #@
-                pass""")
-        with self.assertAddsMessages(testutils.Message('unicode-method', node=node)):
-            self.checker.visit_function(node)
-
-    def test_cmp_method(self):
-        node = test_utils.extract_node("""
-        class Foo(object):
-            def next(self):  #@
-                pass""")
-        with self.assertAddsMessages(testutils.Message('next-method', node=node)):
-            self.checker.visit_function(node)
-
-    def test_builtin_false_positive(self):
-        node = test_utils.build_module('long = int; long  #@')
-        with self.assertNoMessages():
-            self.checker.visit_name(node)
+    CHECKER_CLASS = checker.StrictPython3Checker
 
     def check_not_builtin(self, builtin_name, message):
         node = test_utils.extract_node(builtin_name + '  #@')
@@ -104,52 +38,12 @@ class SixCheckerTest(testutils.CheckerTestCase):
             self.checker.visit_name(node)
 
     @python2_only
-    def test_buffer_builtin(self):
-        self.check_not_builtin('buffer', 'buffer-builtin')
-
-    @python2_only
-    def test_apply_builtin(self):
-        self.check_not_builtin('apply', 'apply-builtin')
-
-    @python2_only
-    def test_cmp_builtin(self):
-        self.check_not_builtin('cmp', 'cmp-builtin')
-
-    @python2_only
-    def test_file_builtin(self):
-        self.check_not_builtin('file', 'file-builtin')
-
-    @python2_only
-    def test_raw_input_builtin(self):
-        self.check_not_builtin('raw_input', 'raw_input-builtin')
-
-    @python2_only
-    def test_long_builtin(self):
-        self.check_not_builtin('long', 'long-builtin')
-
-    @python2_only
-    def test_coerce_builtin(self):
-        self.check_not_builtin('coerce', 'coerce-builtin')
-
-    @python2_only
-    def test_execfile_builtin(self):
-        self.check_not_builtin('execfile', 'execfile-builtin')
-
-    @python2_only
-    def test_xrange_builtin(self):
-        self.check_not_builtin('xrange', 'xrange-builtin')
-
-    @python2_only
-    def test_unicode_builtin(self):
-        self.check_not_builtin('unicode', 'unicode-builtin')
-
-    @python2_only
-    def test_StandardError(self):
-        self.check_not_builtin('StandardError', 'standarderror-builtin')
-
-    @python2_only
     def test_map_builtin(self):
         self.check_not_builtin('map', 'map-builtin')
+
+    @python2_only
+    def test_range_builtin(self):
+        self.check_not_builtin('range', 'range-builtin')
 
     @python2_only
     def test_zip_builtin(self):
@@ -162,63 +56,6 @@ class SixCheckerTest(testutils.CheckerTestCase):
     @python2_only
     def test_open_builtin(self):
         self.check_not_builtin('open', 'open-builtin')
-
-    def test_division(self):
-        node = test_utils.extract_node('3 / 2  #@')
-        with self.assertAddsMessages(testutils.Message('division', node=node)):
-            self.checker.visit_binop(node)
-
-    def test_division_with_future_statement(self):
-        module = test_utils.build_module('from __future__ import division; 3 / 2')
-        with self.assertNoMessages():
-            self.walk(module)
-
-    def test_floor_division(self):
-        node = test_utils.extract_node(' 3 // 2  #@')
-        with self.assertNoMessages():
-            self.checker.visit_binop(node)
-
-    def test_division_by_float(self):
-        left_node = test_utils.extract_node('3.0 / 2 #@')
-        right_node = test_utils.extract_node(' 3 / 2.0  #@')
-        with self.assertNoMessages():
-            for node in (left_node, right_node):
-                self.checker.visit_binop(node)
-
-    def test_relative_import(self):
-        node = test_utils.extract_node('import string  #@')
-        with self.assertAddsMessages(testutils.Message('no-absolute-import', node=node)):
-            self.checker.visit_import(node)
-
-    def test_relative_from_import(self):
-        node = test_utils.extract_node('from os import path  #@')
-        with self.assertAddsMessages(testutils.Message('no-absolute-import', node=node)):
-            self.checker.visit_import(node)
-
-    def test_absolute_import(self):
-        module_import = test_utils.build_module('from __future__ import absolute_import; import os')
-        module_from = test_utils.build_module('from __future__ import absolute_import; from os import path')
-        with self.assertNoMessages():
-            for module in (module_import, module_from):
-                self.walk(module)
-
-    @python2_only
-    def test_exec(self):
-        node = test_utils.extract_node('exec "2 + 3"  #@')
-        with self.assertAddsMessages(testutils.Message('exec-statement', node=node)):
-            self.checker.visit_exec(node)
-
-    def test_metaclass_attr(self):
-        node = test_utils.extract_node('class Foo(object):\n __metaclass__ = type  #@')
-        with self.assertAddsMessages(testutils.Message('metaclass-attribute', node=node)):
-            self.checker.visit_assign(node)
-
-    @python2_only
-    def test_parameter_unpacking(self):
-        node = test_utils.extract_node('def func((a, b)):#@\n pass')
-        arg = node.args.args[0]
-        with self.assertAddsMessages(testutils.Message('parameter-unpacking', node=arg)):
-            self.walk(node)
 
 
 class UnicodeCheckerTest(testutils.CheckerTestCase):
@@ -260,37 +97,6 @@ class UnicodeCheckerTest(testutils.CheckerTestCase):
         module = u'#! /usr/bin/python2.7\n"""Module docstring"""\nfrom __future__ import unicode_literals'
         tokens = self.tokenize(module)
         with self.assertNoMessages():
-            self.checker.process_tokens(tokens)
-
-
-class SyntaxCheckerTest(testutils.CheckerTestCase):
-
-    CHECKER_CLASS = checker.SyntaxChecker
-
-    def tokenize(self, source):
-        return tokenize.generate_tokens(io.StringIO(source).readline)
-
-    @unittest.skipIf(sys.version_info[0] > 2, 'Python 2 only')
-    def test_py2_octal_literal(self):
-        tokens = self.tokenize(u'012')
-        with self.assertAddsMessages(testutils.Message('octal-literal', line=1)):
-            self.checker.process_tokens(tokens)
-
-    def test_py3_octal_literal(self):
-        tokens = self.tokenize(u'0o12')
-        with self.assertNoMessages():
-            self.checker.process_tokens(tokens)
-
-    @unittest.skipIf(sys.version_info[0] > 2, 'Python 2 only')
-    def test_long_L(self):
-        tokens = self.tokenize(u'123L')
-        with self.assertAddsMessages(testutils.Message('long-literal', line=1)):
-            self.checker.process_tokens(tokens)
-
-    @unittest.skipIf(sys.version_info[0] > 2, 'Python 2 only')
-    def test_long_l(self):
-        tokens = self.tokenize(u'123l')
-        with self.assertAddsMessages(testutils.Message('long-literal', line=1)):
             self.checker.process_tokens(tokens)
 
 
